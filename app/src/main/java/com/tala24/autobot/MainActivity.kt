b309845228ff7720ba49123e60d8264582a8cc82
behzadk0701-ship-it <behzad.k.0701@gmail.com>
2026-04-28 00:32:42 +0330

    Update MainActivity.kt

--- app/src/main/java/com/tala24/autobot/MainActivity.kt
+++ app/src/main/java/com/tala24/autobot/MainActivity.kt
@@ -1,1 +1,37 @@
+package com.tala24.autobot
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.work.*
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // دکمه برای تست دستی یک بار
+        findViewById<Button>(R.id.btnSend).setOnClickListener {
+            val req = OneTimeWorkRequestBuilder<AutoSendWorker>().build()
+            WorkManager.getInstance(this).enqueue(req)
+        }
+
+        // زمان‌بندی خودکار هر ۲ ساعت
+        schedulePeriodicWork()
+    }
+
+    private fun schedulePeriodicWork() {
+        val request =
+            PeriodicWorkRequestBuilder<AutoSendWorker>(2, TimeUnit.HOURS)
+                .build()
+
+        WorkManager.getInstance(this)
+            .enqueueUniquePeriodicWork(
+                "AutoSender",
+                ExistingPeriodicWorkPolicy.UPDATE,
+                request
+            )
+    }
+}
